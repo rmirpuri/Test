cb74fb1185ee2f7cfc2166ecfb64cbaccd639520
Rajiv Mirpuri <rajiv.mirpuri@oracle.com>
2016-02-17 13:28:09 -0500

    file in master to create conflit

--- file.rb
+++ file.rb
@@ -1,6 +1,1 @@
-trying to create merge confict, writing this when in master branch.
-merge conflict. Doing this when in local branch.
-more blah blah
-blah
-will run command git diff HEAD
-this is test file. Adding more after commit. Adding more after creating new anotherfile.rb.
+Adding new line in master to create conflict.
